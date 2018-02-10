b7120df5c89c06127d876dbef79a431289d9acc8
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2018-02-10 08:24:33 -0600

    Spring Initialzr

---
+++ src/main/kotlin/hm/binkley/labs/spudnik/App.kt
@@ -0,0 +1,11 @@
+package hm.binkley.labs.spudnik
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
